1e85b8a7be7f7dfafc180db236612d51098e3a3e
Adam Richeimer <adamzr@gmail.com>
2019-08-04 17:06:02 -0700

    Bath update

--- src/app/appointments/appointment-time.ts
+++ src/app/appointments/appointment-time.ts
@@ -3,6 +3,7 @@ export class AppointmentTime {
   constructor(
     public date: string,
     public time: string,
+    public roomType: string,
     public paymentMethod: string,
     public notes: string
   ) {  }
