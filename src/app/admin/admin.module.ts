62fdb7d93d194da2cfcf808eec8ba9cdefa7f5d7
Miriam Striks <striks.miriam@gmail.com>
2021-11-11 11:54:31 -0800

    Add date picker to admin page
    Allow admins to view the schedule for a given day instead of only displaying the current day's schedule. The current day's schedule will be displayed by default.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {AdminComponent} from './admin.component';
-import {AdminRoutingModule} from './admin-routing.module';
-import {AppointmentsService} from '../appointments/appointments.service';
+import { AdminComponent } from './admin.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AppointmentsService } from '../appointments/appointments.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatDatepickerModule
   ],
   declarations: [
     AdminComponent
